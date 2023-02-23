6069a81d118d46736fc6481d3763646419dbe562
liuhanqiang <1126045115@qq.com>
2023-02-23 21:07:52 +0800

    222 提交

--- src/main/java/com/ruoyi/project/system/domain/SysDept.java
+++ src/main/java/com/ruoyi/project/system/domain/SysDept.java
@@ -97,7 +97,7 @@ public class SysDept extends BaseEntity
         this.deptName = deptName;
     }
 
-    @NotNull(message = "显示顺序不能为空11111")
+    @NotNull(message = "显示顺序不能为空1111222")
     public Integer getOrderNum()
     {
         return orderNum;
